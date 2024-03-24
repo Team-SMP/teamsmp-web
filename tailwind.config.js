a0c29256a71639f481b79cb01634f343a2d250cc
JunglTemple <tubikardu@gmail.com>
2024-03-24 13:14:31 +0000

    Get main stuff sorted

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		fontFamily: {
+			sans: ["Inter", "Helvetica", "Arial", "sans-serif"],
+			title: ['"Space Grotesk"', "Inter", "Helvetica", "Arial", "sans-serif"],
+			mono: ['"JetBrains Mono"', "Consolas", '"Courier New"', "monospace"],
+		},
+	},
+	plugins: [],
+};
