35440c30a2715b095410961991bcd3bea636d25c
JunglTemple <146572102+JunglTemple@users.noreply.github.com>
2024-03-25 17:15:43 +0000

    Add Google Search Console verif

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <svelte:head>
 	<title>Team SMP: Home</title>
 	<meta name="description" content="The Team SMP website." />
+	<meta name="google-site-verification" content="QUVXV8P_tfaaJHEBF_YaV3Ji7woT0QkLnMNRnFIwZEY" />
 </svelte:head>
 
 <script lang="ts">
